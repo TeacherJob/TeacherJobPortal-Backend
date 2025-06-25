27858e557aab6c62ce4f85fc2a8cca17aa65ea40
adarsh-cmtai <adarsh.shahi@completrix.com>
2025-06-25 13:29:24 +0530

    Update auth.routes.js

--- auth/routes/auth.routes.js
+++ auth/routes/auth.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/google', googleLogin);
-router.get('/logout', logout);
+router.post('/logout', logout);
 router.get('/me', protect, getMe);
 router.put('/updatepassword', protect, updatePassword);
 router.delete('/deleteaccount', protect, deleteAccount);
