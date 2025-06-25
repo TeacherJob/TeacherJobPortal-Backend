29842856e3686dbb24872529810e621e6693cd39
adarsh-cmtai <adarsh.shahi@completrix.com>
2025-06-25 13:05:44 +0530

    Update auth.controller.js

--- auth/controllers/auth.controller.js
+++ auth/controllers/auth.controller.js
@@ -21,6 +21,7 @@ const sendTokenResponse = (user, statusCode, res) => {
         expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
+        sameSite: 'Lax',
     };
     res.status(statusCode)
         .cookie('token', token, options)
