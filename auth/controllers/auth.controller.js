1d21c20f0ea66f5fcee19b5ab7541744dc5413b2
adarsh-cmtai <adarsh.shahi@completrix.com>
2025-06-25 13:13:46 +0530

    Update auth.controller.js

--- auth/controllers/auth.controller.js
+++ auth/controllers/auth.controller.js
@@ -121,10 +121,13 @@ export const googleLogin = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-    res.cookie('token', 'none', {
-        expires: new Date(Date.now() + 10 * 1000),
+    res.cookie('token', '', {
         httpOnly: true,
+        expires: new Date(0),
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: process.env.NODE_ENV === 'production'
     });
+
     res.status(200).json({ success: true, message: 'Logged out successfully' });
 };
 
